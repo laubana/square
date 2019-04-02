5f322bcd634f067b330fd43bffe14b396126d508
laubana <laubana@gmail.com>
2019-04-02 09:09:59 +0900

    20190402

---
+++ square/src/main/java/project/ppaya/square/yhvo/Group.java
@@ -0,0 +1,62 @@
+package project.ppaya.square.yhvo;
+
+public class Group
+{
+	int group_id;
+	int group_category_id;
+	String region;
+	String leader;
+	String name;
+	String content;
+	String group_logo;
+	String group_image;
+	public Group(){}
+	public int getGroup_id() {
+		return group_id;
+	}
+	public void setGroup_id(int group_id) {
+		this.group_id = group_id;
+	}
+	public int getGroup_category_id() {
+		return group_category_id;
+	}
+	public void setGroup_category_id(int group_category_id) {
+		this.group_category_id = group_category_id;
+	}
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public String getLeader() {
+		return leader;
+	}
+	public void setLeader(String leader) {
+		this.leader = leader;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getGroup_logo() {
+		return group_logo;
+	}
+	public void setGroup_logo(String group_logo) {
+		this.group_logo = group_logo;
+	}
+	public String getGroup_image() {
+		return group_image;
+	}
+	public void setGroup_image(String group_image) {
+		this.group_image = group_image;
+	}
+}
---
+++ square/src/main/java/project/ppaya/square/yhvo/UserSchedule.java
@@ -0,0 +1,41 @@
+package project.ppaya.square.yhvo;
+
+public class UserSchedule
+{
+	int event_id;
+	int group_id;
+	String host_id;
+	String name;
+	String content;
+	public UserSchedule(){}
+	public int getEvent_id() {
+		return event_id;
+	}
+	public void setEvent_id(int event_id) {
+		this.event_id = event_id;
+	}
+	public int getGroup_id() {
+		return group_id;
+	}
+	public void setGroup_id(int group_id) {
+		this.group_id = group_id;
+	}
+	public String getHost_id() {
+		return host_id;
+	}
+	public void setHost_id(String host_id) {
+		this.host_id = host_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
