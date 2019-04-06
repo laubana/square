53add47b515bf0daabd3123bbd627e51e98d93da
parksay <shsh9156@naver.com>
2019-04-06 13:18:00 +0900

    -

--- square/src/main/java/project/ppaya/square/controller/MainController.java
+++ square/src/main/java/project/ppaya/square/controller/MainController.java
@@ -1,22 +1,52 @@
 package project.ppaya.square.controller;
 
+import java.util.ArrayList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import project.ppaya.square.shdao.SH_DAO_Group;
+import project.ppaya.square.vo.GroupCategory;
+
+
 
 @Controller
 public class MainController
 {
+	
+@Autowired
+SH_DAO_Group sh_gdao;
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
 	@RequestMapping(value = "main", method = RequestMethod.GET)
-	public String mainForm()
+	public String mainForm(Model request)
 	{
-		logger.info("메인입니다!");
-
+		//카테고리 리스트 받아 옴
+		ArrayList<GroupCategory> clist = null;
+		clist = sh_gdao.getCategoryList();
+		int i = 0;
+		if(clist != null) {
+			request.addAttribute("clist",clist);
+			for(i = 1 ; i <= 5; i = i +1){
+				clist.add(new GroupCategory(i,"temp_name"+i));
+			}
+			
+			logger.info("메인입니다!");
+		} 
+		
+		else {
+			GroupCategory g1 = new GroupCategory("오류입니다");
+			clist = new ArrayList<GroupCategory>();
+			clist.add(g1);
+			request.addAttribute("clist", clist);
+		}
 		return "main/mainForm";
+
 	}
-	
+
 }
